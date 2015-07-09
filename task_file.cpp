7aba96b53574c05fccb1cb99cda3c2d28f115baf
shahmeerarshad <shahmeer.arshad@coeus-solutions.de>
2015-07-09 13:29:37 +0500

    Update task_file.cpp

--- task_file.cpp
+++ task_file.cpp
@@ -1,9 +1,14 @@
+//Print the elements of a linked list
+//https://www.hackerrank.com/challenges/print-the-elements-of-a-linked-list
+//https://www.hackerrank.com/shahmeerarshad
+
+  
   struct Node
   {
      int data;
      struct Node *next;
   }
-
+  
 void Print(Node *head)
 {
     Node *temp;
